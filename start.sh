38a917cbb7e9ca30468f8c070a9d52e06e995273
homanxzd55-ship-it <homanxzd55@gmail.com>
2026-06-06 19:07:15 +0330

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,2 @@
 #!/bin/bash
-python -c "
-import os, time, telebot
-TOKEN = os.environ.get('TELEGRAM_TOKEN')
-RENDER_URL = os.environ.get('RENDER_URL')
-bot = telebot.TeleBot(TOKEN)
-bot.remove_webhook()
-time.sleep(2)
-result = bot.set_webhook(url=f'{RENDER_URL}/{TOKEN}')
-print(f'Webhook set: {result}', flush=True)
-"
-gunicorn bot:app --bind 0.0.0.0:$PORT
+python bot.py
